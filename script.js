cda6ee9486e84a81a2a9e8a4c239b965b24daafe
RBoghian <boghian_razvan@yahoo.com>
2022-05-30 18:48:42 +0300

    Link to API created

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
+ const imageContainer = document.getElementById('image-container');
+ const loader = document.getElementById('loader');
+
+ let photosArray = [];
+ 
+ 
+ //Unsplash API
+ const count=10;
+ const apiKey='hleyLidfuuJnSg9cCyKeMuCwr-I781kYi0vI6kpcwug';
+ const apiUrl='https://api.unsplash.com/photos/random?client_id='+apiKey+'&count='+count;
+
+ 
+ //Helper Function to set attributes on DOM Elements
+ function setAttributes(element, attributes){
+     for(const key in attributes ){
+         element.setAttribute(key,attributes[key]);
+     }
+ }
+ 
+ // Create elements for links and photos
+ function displayPhotos(){
+     //run function for each object in photosArray
+     photosArray.forEach((photo) => {
+        // Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        //Create <img> for photo
+        const img = document.createElement('img');
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+        //Put <img> inside <a>, then put both inside imageContainer
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+     })
+ }
+ 
+ //Get photos from Unsplash API
+ async function getPhotos(){
+     try{
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+     }catch(error){
+        
+     }
+    }
+     // On Load
+     getPhotos();
  
